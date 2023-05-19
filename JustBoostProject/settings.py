465da9cb669c61650889dce1764420f24f81aa02
LolAnthony <126456756+LolAnthony@users.noreply.github.com>
2023-05-20 02:07:04 +0300

    DB completely

--- JustBoostProject/settings.py
+++ JustBoostProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'JustBoostProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'sqlprojectdb',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
